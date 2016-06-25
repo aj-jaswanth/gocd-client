a9d912d83bcc2b751b11c6da171e5d116f556fea
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-06-25 16:58:57 +0530

    WIP : Add support for update host name api

--- src/gocd/gocd.go
+++ src/gocd/gocd.go
@@ -1,7 +1,9 @@
 package gocd
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -17,8 +19,8 @@ type auth struct {
 var gocdAuth auth
 
 type Agent struct {
-	Uuid, Agentname, Ip_address, Os, Free_space, Status, Sandbox string
-	Resources, Environments                                      []string
+	Uuid, Agent_name, Ip_address, Os, Free_space, Status, Sandbox, Hostname string
+	Resources, Environments                                                 []string
 }
 
 func (gocd *GoCd) SetAuth(username, password string) {
@@ -28,6 +30,7 @@ func (gocd *GoCd) SetAuth(username, password string) {
 func (gocd *GoCd) GetAgents() (agents []Agent) {
 	request, ok := http.NewRequest(http.MethodGet, gocd.Url+"/agents", nil)
 	request.SetBasicAuth(gocdAuth.username, gocdAuth.password)
+
 	client := http.Client{}
 	response, ok := client.Do(request)
 	if ok != nil {
@@ -36,5 +39,21 @@ func (gocd *GoCd) GetAgents() (agents []Agent) {
 
 	body, ok := ioutil.ReadAll(response.Body)
 	json.Unmarshal(body, &agents)
-	return agents
+	return
+}
+
+func (gocd *GoCd) UpdateAgentHostName(agent Agent, hostname string) (updatedAgent Agent) {
+	request, ok := http.NewRequest(http.MethodPatch, gocd.Url+"/agents/"+agent.Uuid, bytes.NewReader([]byte(`{"hostname":"`+hostname+`"}`)))
+	request.SetBasicAuth(gocdAuth.username, gocdAuth.password)
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Accept", "application/vnd.go.cd.v2+json")
+
+	client := http.Client{}
+	response, ok := client.Do(request)
+	if ok != nil {
+		return
+	}
+	body, ok := ioutil.ReadAll(response.Body)
+	json.Unmarshal(body, &updatedAgent)
+	return
 }
